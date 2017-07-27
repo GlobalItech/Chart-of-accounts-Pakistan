892f64fb7f9c33bd462260993867793d386cfd08
GlobalItech <30366468+GlobalItech@users.noreply.github.com>
2017-07-27 12:48:31 +0500

    Update __manifest__.py

--- MFG_COA_PAK/__manifest__.py
+++ MFG_COA_PAK/__manifest__.py
@@ -8,19 +8,6 @@
 ##############################################################################
 {
     'name': "Pakistan Chart of Accounts (Manufacturing)",
-    'summary': """
-        Adds Parent account and ability to open chart of account list view based on the date and moves""",
-    'description': """
-
-        * Manufacturing Chart Of  Accounts
-        * Adds parent account in account
-        * Adds new type 'view' in account type
-        * Adds Chart of account heirachy view
-        * Adds credit, debit and balance in account
-        * Shows chart of account based on the date and target moves we have selected
-    - Need to set the group show chart of account structure to view the chart of account heirarchy.
-    """,
-
     'author': 'Itech Resources',
     'website': 'http://itechresources.net/',
     'category': 'Accounting &amp; Finance',
